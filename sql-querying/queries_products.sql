94c9fdabd10eb76b2c773c5ace42818598b23e39
Kyamran Rizayev <kyamranr@gmail.com>
2024-02-15 19:40:21 -0600

    added all folders

---
+++ sql-querying/queries_products.sql
@@ -0,0 +1,7 @@
+-- Comments in SQL Start with dash-dash --
+
+-- Add a product to the table with the name of “chair”, price of 44.00, and 
+-- can_be_returned of false.
+
+INSERT INTO product (name, price, can_be_returned)
+    VALUES ('chair', 44.00, 'f');
